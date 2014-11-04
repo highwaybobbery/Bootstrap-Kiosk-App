3e53bd23aed7060fd24313a599305b64a462d64c
John Brosnan <jbrosnan@constantcontact.com>
2014-11-03 22:40:55 -0500

    wip: some login stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 BootstrapKioskApp::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
+  root to: 'home#main'
 end
